fe90eb233f52e2814fb23f5f623a7784e2c24a1e
jonmersha@gmail.com <jonmersha@gmail.com>
2025-10-10 21:13:47 +0300

    commentos

--- src/lib/apiEndpoints.ts
+++ src/lib/apiEndpoints.ts
@@ -1,25 +1,25 @@
- const BASE_URL = "https://m.besheger.com";
-//const BASE_URL = "http://localhost:8000";
+ //const BASE_URL = "https://m.besheger.com";
+const BASE_URL = "http://localhost:8000";
 
 export const API = {
   LOGIN: `${BASE_URL}/auth/jwt/create`,
   ME: `${BASE_URL}/auth/users/me/`,
-  CUSTOMERS: `${BASE_URL}/customers/`,
-  COMPANIES: `${BASE_URL}/companies/`,
-  FACTORIES: `${BASE_URL}/factories/`,
-  WAREHOUSES: `${BASE_URL}/warehouses/`,
-  CATEGORIES: `${BASE_URL}/categories/`,
-  PRODUCTS: `${BASE_URL}/products/`,
-  STOCKS: `${BASE_URL}/stocks/`,
-  STOCK_MOVEMENTS: `${BASE_URL}/stock-movements/`,
-  SUPPLIERS: `${BASE_URL}/suppliers/`,
-  PURCHASE_ORDERS: `${BASE_URL}/purchase-orders/`,
-  SALES_ORDERS: `${BASE_URL}/sales-orders/`,
-  INVOICES: `${BASE_URL}/invoices/`,
-  PAYMENT_METHODS: `${BASE_URL}/payment-methods/`,
-  PAYMENTS: `${BASE_URL}/poso/payments/`,
-  CITIES: `${BASE_URL}/core/cities/`,
-  ADMIN_REGIONS: `${BASE_URL}/core/admin-regions/`,
+  // CUSTOMERS: `${BASE_URL}/customers/`,
+  // COMPANIES: `${BASE_URL}/companies/`,
+  // FACTORIES: `${BASE_URL}/factories/`,
+  // WAREHOUSES: `${BASE_URL}/warehouses/`,
+  // CATEGORIES: `${BASE_URL}/categories/`,    
+  // PRODUCTS: `${BASE_URL}/products/`,
+  // STOCKS: `${BASE_URL}/stocks/`,
+  // STOCK_MOVEMENTS: `${BASE_URL}/stock-movements/`,
+  // SUPPLIERS: `${BASE_URL}/suppliers/`,
+  // PURCHASE_ORDERS: `${BASE_URL}/purchase-orders/`,
+  // SALES_ORDERS: `${BASE_URL}/sales-orders/`,
+  // INVOICES: `${BASE_URL}/invoices/`,
+  // PAYMENT_METHODS: `${BASE_URL}/payment-methods/`,
+  // PAYMENTS: `${BASE_URL}/poso/payments/`,
+  // CITIES: `${BASE_URL}/core/cities/`,
+  // ADMIN_REGIONS: `${BASE_URL}/core/admin-regions/`,
 };
 
 // Base URLs
